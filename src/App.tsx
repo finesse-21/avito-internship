7de3ca8ea2d102b0a7ebf06445f655f408499acd
Egor Alekseenko <21pepperforreal@gmail.com>
2025-11-14 23:16:12 +0300

    feat: setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import AdsList from './pages/AdsList';
+import AdDetail from './pages/AdDetail';
+import Stats from './pages/Stats';
+import MainLayout from './components/Layout/MainLayout';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ConfigProvider locale={ruRU}>
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/list" replace />} />
+            <Route path="/list" element={<AdsList />} />
+            <Route path="/item/:id" element={<AdDetail />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="*" element={<Navigate to="/list" replace />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
